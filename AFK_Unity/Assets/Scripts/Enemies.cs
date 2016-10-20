eb8b26a9aef38e41faaee31ad9aed1e909a17eed
Brian Pullen <brian.p.pullen@gmail.com>
2016-10-20 10:01:55 -0700

    Initial creation of unity projects with a few folders.

---
+++ AFK_Unity/Assets/Scripts/Enemies.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemies : MonoBehaviour {
+
+
+	public int enemyCount;
+
+	// Use this for initialization
+	void Start () 
+	{
+		enemyCount = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
